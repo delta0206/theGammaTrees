37412867febd6bb26a211848c54a71e176ba2413
delta0206 <delta.gammatrees@gmail.com>
2023-02-09 22:33:46 +0900

    feat: ➕ Add some packages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
+
+const withAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const withPlugins = require('next-compose-plugins');
+const withPWA = require('next-pwa')({
+  dest: './public',
+});
+
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = process.env.NODE_ENV === 'production' ? withPlugins([withPWA, withAnalyzer], nextConfig) : nextConfig;
